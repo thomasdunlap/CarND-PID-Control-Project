ba4a5ba749fe4bd718c4ee9b5d00363a9c57abc6
thomasdunlap <tom.a.dunlap@gmail.com>
2018-03-09 23:01:21 -0500

    feat: Completed PID init in PID.cpp.

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,15 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  p_error = cte;
+  d_error = cte - p_error;
 }
 
 double PID::TotalError() {
 }
-
