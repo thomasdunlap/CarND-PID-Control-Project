dce0c9129eea6b2b233bf90107f6d83e2c2dcc85
thomasdunlap <tom.a.dunlap@gmail.com>
2018-04-09 21:24:09 -0400

    refactor: Changed main.cpp pid.Init values to 0.2, 0.004, 2.9 - working model.

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // Initialize the pid variable.
-  pid.Init(0.3, 0.005, 2.9);
+  pid.Init(0.2, 0.004, 2.9);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
